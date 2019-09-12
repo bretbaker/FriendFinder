419894345e4e2a1efd1d1bba61cba55d1c098201
Bret Baker <bretbaker808@gmail.com>
2019-09-12 13:41:03 -0700

    logic complete, modal complete, need to setup server and clean up appearance of code

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,15 +2,15 @@
 let friends = [];
 let newFriend = new Friend('Jimmy Jones', 'https://i.pravatar.cc/150?img=70', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 friends.push(newFriend);
+newFriend = new Friend('Lisa Lurrington', 'https://i.pravatar.cc/150?img=44', 4, 4, 4, 4, 4, 4, 4, 4, 4, 4);
+friends.push(newFriend);
 newFriend = new Friend('Ronnie Wilson', 'https://i.pravatar.cc/150?img=65', 3, 3, 3, 3, 3, 3, 3, 3, 2, 2);
+friends.push(newFriend);
+newFriend = new Friend('Jessica Fischer', 'https://i.pravatar.cc/150?img=36', 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
 friends.push(newFriend);
 newFriend = new Friend('Kate Banta', 'https://i.pravatar.cc/150?img=49', 2, 2, 2, 2, 2, 2, 2, 2, 2, 2);
 friends.push(newFriend);
 newFriend = new Friend('Larry Oswiler', 'https://i.pravatar.cc/150?img=66', 3, 3, 3, 3, 3, 3, 3, 3, 3, 3);
-friends.push(newFriend);
-newFriend = new Friend('Lisa Lurrington', 'https://i.pravatar.cc/150?img=44', 4, 4, 4, 4, 4, 4, 4, 4, 4, 4);
-friends.push(newFriend);
-newFriend = new Friend('Jessica Fischer', 'https://i.pravatar.cc/150?img=36', 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
 friends.push(newFriend);
 console.log(friends);
 
@@ -19,37 +19,87 @@ function Friend(name, image, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) {
   this.name = name;
   this.photo = image;
   this.score = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10];
+  this.scoreSum = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10;
 };
 
 // function that sums scores in friends objects
-let sumArr = [];
-const sumScore = () => {
-  for (let i = 0; i < friends.length; i++) {
-    let sum = 0;
-    for (let j = 0; j < friends[i].score.length; j++) {
-       sum += friends[i].score[j];
-    }
-    sumArr.push(sum);
-  }
-};
-sumScore();
-console.log(sumArr);
+// let sumArr = [];
+// const sumScore = () => {
+//   for (let i = 0; i < friends.length; i++) {
+//     // let sum = 0;
+//     // for (let j = 0; j < friends[i].score.length; j++) {
+//     //    sum += friends[i].score[j];
+//     // }
+//     // sumArr.push(sum);
+//     console.log(friends[i].scoreSum);
+//   }
+// };
+// sumScore();
+// console.log(sumArr);
 
 // function that produces match by 
-let minDiff = 51;
-let friendMatch;
-const matchMaker = (friend) => {
-  for (let i = 0; i < friends.length; i++) {
-    if (friends[friend].name !== friends[i].name) {
-      if (Math.abs(sumArr[friend] - sumArr[i]) < minDiff) {
-        minDiff = Math.abs(sumArr[friend] - sumArr[i]);
-        friendMatch = friends[i].name;
-      }
-    }
+// let minDiff = 51;
+// let friendMatch;
+// const matchMaker = (friend) => {
+//   for (let i = 0; i < friends.length; i++) {
+//     if (friends[friend].name !== friends[i].name) {
+//       if (Math.abs(sumArr[friend] - sumArr[i]) < minDiff) {
+//         minDiff = Math.abs(sumArr[friend] - sumArr[i]);
+//         friendMatch = friends[i].name;
+//       }
+//     }
+//   }
+//   console.log(friends[friend].name + ', you have matched with ' + friendMatch + '!');
+// };
+// matchMaker(friends.length - 1);
+// matchMaker(friends.length - 2);
+// matchMaker(friends.length - 3);
+
+// const matchMaker = () => {
+//   friends
+// }
+
+// const friendsSort = () => {
+//   friends.sort((a, b) => {
+//     return a.scoreSum - b.scoreSum;
+//   });
+  // console.log(friends);
+  // sumScore();
+// };
+// friendsSort();
+
+let diffArr = [];
+const diffFinder = () => {
+  console.log(friends[friends.length - 1].name);
+  for (let i = 0; i < friends.length - 1; i++) {
+    diffArr.push(Math.abs(friends[friends.length - 1].scoreSum - friends[i].scoreSum));
+    // console.log(friends[i].scoreSum - friends[i + 1].scoreSum);
   }
-  console.log(friends[friend].name + ', you have matched with ' + friendMatch + '!');
+  console.log(diffArr);
 };
-matchMaker(2);
+// diffFinder();
+
+const matchMaker = () => {
+  let matchIndex = diffArr.indexOf(Math.min(...diffArr));
+  console.log(Math.min(...diffArr));
+  console.log(matchIndex);
+  console.log(friends[friends.length - 1].name + ', you have the greatest compatibility with, ' + friends[matchIndex].name + '!');
+  $("#modal-message").text(friends[friends.length - 1].name + ', you have the greatest compatibility with, ' + friends[matchIndex].name + '!');
+  $("#modal-img").attr('src', friends[matchIndex].photo);
+  // if (friends[matchIndex].name === friends[friends.length - 1].name) {
+  //   console.log(friends[matchIndex].name + ', you have the greatest compatibility with, ' + friends[matchIndex + 1].name + '!');
+  //   $("#modal-message").text(friends[matchIndex].name + ', you have the greatest compatibility with, ' + friends[matchIndex + 1].name + '!');
+  // } else {
+  //   console.log(friends[matchIndex + 1].name + ', you have the greatest compatibility with, ' + friends[matchIndex].name + '!');
+  //   $("#modal-message").text(friends[matchIndex + 1].name + ', you have the greatest compatibility with, ' + friends[matchIndex].name + '!');
+  // }
+};
+// matchMaker();
+
+// const modalPopulator = () => {
+//   $()
+// }
+
 
 // Friend constructor utilized on click of submit button; data stored and compared against current database; output produced for user
 $("#submit-button").on('click', () => {
@@ -70,8 +120,14 @@ $("#submit-button").on('click', () => {
   newFriend = new Friend(newName, newImage, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
   friends.push(newFriend);
   console.log(friends);
+  // console.log(friends.length);
+  // console.log(friends.length - 1)
+  // matchMaker(friends.length - 1);
+  // friendsSort();
+  diffFinder();
+  matchMaker();
 
-
+  $("#myModal").css('display', 'block');
 
   $("#new-name").val('');
   $("#new-image").val('');
@@ -85,5 +141,11 @@ $("#submit-button").on('click', () => {
   $("#q-8").val('');
   $("#q-9").val('');
   $("#q-10").val('');
+
+});
+
+$(".close").on('click', () => {
+
+  $("#myModal").css('display', 'none');
 
 });
